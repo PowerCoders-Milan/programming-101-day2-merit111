352815cf86b63d36d1837bda9c35e18768c98bb7
Your Name Here <ebrimaj604@gmail.com>
2021-02-19 14:57:48 +0100

    exercise 2

--- 02-Counting-Characters/counting_characters.js
+++ 02-Counting-Characters/counting_characters.js
@@ -1,8 +1,17 @@
 //insert your pseudocode below
 /*
 
-here
-
+set a variable to bill rate
+create a prompt with input and exert it inside Bill rate
+using the message type any word of your choice
+calculate the number of the characters
+print the number of the rendered words
+using the message given word + has character
 */
 
 //insert your code below
+var Bill_rate;
+
+
+Bill_rate = window.prompt('type the words of your choice');
+window.alert([Bill_rate,' ','Bill rate'.length,''].join(''));
