15d5e60a27e0dc3138f83fafe03ef6b6a0acfe9f
Your Name Here <ebrimaj604@gmail.com>
2021-02-19 12:45:08 +0100

    exercise 1

--- 01-Say-Hello/say_hello.js
+++ 01-Say-Hello/say_hello.js
@@ -1,8 +1,16 @@
 //insert your pseudocode below
 /*
 
-here
+*invent two varaiables, the first will be Ebrima and the second will be hello
+*then will need to place our variable orderly
+*then will promp for my name 
+*print the greetings using my name.
 
 */
 
 //insert your code below
+var Ebrima;
+
+
+Ebrima = window.prompt('Can I please know your name');
+window.alert(['Hello',' ',Ebrima].join(''));
